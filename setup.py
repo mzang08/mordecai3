22e5cb2a4a928c9e28a7f8d934cdfb6da751b5ea
Andreas Beger <adbeger@gmail.com>
2025-02-07 10:29:58 +0200

    fix typo in asset file name

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
                              'assets/feature_code_dict.json',
                              'assets/hierarchy.txt',
                              'assets/mordecai_2023-03-28.pt',
-                             'assets/mordecai_2023-06-04.pt',
+                             'assets/mordecai_2024-06-04.pt',
                              'assets/wikipedia-iso-country-codes.txt']}
 )
 
